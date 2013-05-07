f49dfce460035e18478748898f60f6ebe6b7af3a
matterker <matterker@matterker.com>
2013-05-06 23:16:03 -0500

    Troubleshoot spec issues

--- app/controllers/add_thing_controller.rb
+++ app/controllers/add_thing_controller.rb
@@ -10,6 +10,17 @@ class AddThingController < UIViewController
       UIBarButtonSystemItemCancel, target:self, action:'cancelAdd')
     cancel.accessibilityLabel = 'cancel-add'
     navigationItem.leftBarButtonItem = cancel
+
+    #delete this button; troubleshooting spec not tapping bar button by label
+    @cancel_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @cancel_button.setTitle("cancel-add", forState: UIControlStateNormal)
+    @cancel_button.sizeToFit
+    @cancel_button.frame = CGRect.new(
+      [10, self.view.frame.size.height - 60 - @cancel_button.frame.size.height],
+      @cancel_button.frame.size)
+    self.view.addSubview(@cancel_button)
+    @cancel_button.addTarget(self,
+                             action:"cancelAdd", forControlEvents:UIControlEventTouchUpInside)
   end
 
   def cancelAdd
